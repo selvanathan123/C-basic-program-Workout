3a2001186390be00c63022ab73cf59d2c691c717
“selva” <selvanathan@twilightsoftwares.com>
2023-11-17 13:01:08 +0530

    gaming number

---
+++ C# Basics/Gaming.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Basics
+{
+    public class Gaming
+    {
+        public void game()
+        {
+            while (true)
+            {
+                int randno = Newnum(1, 101);
+                int count = 1;
+                while (true)
+                {
+                    Console.Write("Enter a number between 1 and 100(0 to quit):");
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    if (input == 0)
+                        return;
+                    else if (input < randno)
+                    {
+                        Console.WriteLine("Low, try again.");
+                        ++count;
+                        continue;
+                    }
+                    else if (input > randno)
+                    {
+                        Console.WriteLine("High, try again.");
+                        ++count;
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it! The number was {0}!",
+                                           randno);
+                        Console.WriteLine("It took you {0} {1}.\n", count,
+                                           count == 1 ? "try" : "tries");
+                        break;
+                    }
+                }
+            }
+
+        }
+        static int Newnum(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+    
+}
--- C# Basics/Program.cs
+++ C# Basics/Program.cs
@@ -1,72 +1,78 @@
-﻿using System;
+﻿using C__Basics;
+using System;
 class program
 {
     public static void Main()
     {
-        int createpin, confirmpin;
-        int amount = 1000, deposit, withdraw;
-        int choice, pin = 0, x = 0;
-        Console.WriteLine("Create your pin number");
-        createpin = int.Parse(Console.ReadLine());
-        Console.WriteLine("Confirm your pin number");
-        confirmpin = int.Parse(Console.ReadLine());
-        if (createpin == confirmpin)
-        {
-            Console.WriteLine("Enter Your Pin Number ");
-            pin = int.Parse(Console.ReadLine());
-            if (confirmpin == pin)
-            {
-                while (true)
-                {
-                    Console.WriteLine("********Welcome to ATM Service**************\n");
-                    Console.WriteLine("1. Check Balance\n");
-                    Console.WriteLine("2. Withdraw Cash\n");
-                    Console.WriteLine("3. Deposit Cash\n");
-                    Console.WriteLine("4. Quit\n");
-                    Console.WriteLine("*********************************************\n\n");
-                    Console.WriteLine("Enter your choice: ");
-                    choice = int.Parse(Console.ReadLine());
-                    switch (choice)
-                    {
-                        case 1:
-                            Console.WriteLine("\n YOUR BALANCE IN Rs : {0} ", amount);
-                            break;
-                        case 2:
-                            Console.WriteLine("\n ENTER THE AMOUNT TO WITHDRAW: ");
-                            withdraw = int.Parse(Console.ReadLine());
-                            if (withdraw % 100 != 0)
-                            {
-                                Console.WriteLine("\n PLEASE ENTER THE AMOUNT IN MULTIPLES OF 100");
-                            }
-                            else if (withdraw > (amount - 500))
-                            {
-                                Console.WriteLine("\n INSUFFICENT BALANCE");
-                            }
-                            else
-                            {
-                                amount = amount - withdraw;
-                                Console.WriteLine("\n\n PLEASE COLLECT CASH");
-                                Console.WriteLine("\n YOUR CURRENT BALANCE IS {0}", amount);
-                            }
-                            break;
-                        case 3:
-                            Console.WriteLine("\n ENTER THE AMOUNT TO DEPOSIT");
-                            deposit = int.Parse(Console.ReadLine());
-                            amount = amount + deposit;
-                            Console.WriteLine("YOUR BALANCE IS {0}", amount);
-                            break;
-                        case 4:
-                            Console.WriteLine("\n THANK U USING ATM");
-                            break;
-                    }
-                }
-                Console.WriteLine("\n\n THANKS FOR USING OUT ATM SERVICE");
-            }
-            else Console.WriteLine("please enter the valid pin number");
-        }
-        else
-        {
-            Console.WriteLine("Confirm pin incorrect");
-        }
+        Gaming ga = new Gaming();
+        ga.game();
+        //int createpin, confirmpin;
+        //int amount = 1000, deposit, withdraw;
+        //int choice, pin = 0, x = 0;
+        //Console.WriteLine("Create your pin number");
+        //createpin = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Confirm your pin number");
+        //confirmpin = int.Parse(Console.ReadLine());
+        
+        //if (createpin == confirmpin)
+        //{
+        
+
+        //    Console.WriteLine("Enter Your Pin Number ");
+        //    pin = int.Parse(Console.ReadLine());
+        //    if (confirmpin == pin)
+        //    {
+        //        while (true)
+        //        {
+        //            Console.WriteLine("********Welcome to ATM Service**************\n");
+        //            Console.WriteLine("1. Check Balance\n");
+        //            Console.WriteLine("2. Withdraw Cash\n");
+        //            Console.WriteLine("3. Deposit Cash\n");
+        //            Console.WriteLine("4. Quit\n");
+        //            Console.WriteLine("*********************************************\n\n");
+        //            Console.WriteLine("Enter your choice: ");
+        //            choice = int.Parse(Console.ReadLine());
+        //            switch (choice)
+        //            {
+        //                case 1:
+        //                    Console.WriteLine("\n YOUR BALANCE IN Rs : {0} ", amount);
+        //                    break;
+        //                case 2:
+        //                    Console.WriteLine("\n ENTER THE AMOUNT TO WITHDRAW: ");
+        //                    withdraw = int.Parse(Console.ReadLine());
+        //                    if (withdraw % 100 != 0)
+        //                    {
+        //                        Console.WriteLine("\n PLEASE ENTER THE AMOUNT IN MULTIPLES OF 100");
+        //                    }
+        //                    else if (withdraw > (amount - 500))
+        //                    {
+        //                        Console.WriteLine("\n INSUFFICENT BALANCE");
+        //                    }
+        //                    else
+        //                    {
+        //                        amount = amount - withdraw;
+        //                        Console.WriteLine("\n\n PLEASE COLLECT CASH");
+        //                        Console.WriteLine("\n YOUR CURRENT BALANCE IS {0}", amount);
+        //                    }
+        //                    break;
+        //                case 3:
+        //                    Console.WriteLine("\n ENTER THE AMOUNT TO DEPOSIT");
+        //                    deposit = int.Parse(Console.ReadLine());
+        //                    amount = amount + deposit;
+        //                    Console.WriteLine("YOUR BALANCE IS {0}", amount);
+        //                    break;
+        //                case 4:
+        //                    Console.WriteLine("\n THANK U USING ATM");
+        //                    break;
+        //            }
+        //        }
+        //        Console.WriteLine("\n\n THANKS FOR USING OUT ATM SERVICE");
+        //    }
+        //    else Console.WriteLine("please enter the valid pin number");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Confirm pin incorrect");
+        //}
     }
 }
